f6715beb95a6c6a74a8622359cf72e27c15e2a39
SamuelR827 <srrantz17@gmail.com>
2024-04-02 10:30:47 -0400

    fix login issue

--- routes/users.js
+++ routes/users.js
@@ -37,11 +37,12 @@ router.get('/logout', function(req, res, next){
 }); 
 
 
+
 function loggedIn(req, res, next) {
   if (req.user) {
     next(); // req.user exists, go to the next function (right after loggedIn)
   } else {
-    res.redirect('/users/login'); // user doesn't exists, redirect to localhost:3000/users/login
+    res.redirect('/users/login'); // user doesn't exists redirect to localhost:3000/users/login
   }
 }
 
@@ -67,18 +68,19 @@ router.get('/login', notLoggedIn, function(req, res){
 
 // localhost:3000/users/login
 router.post('/login',
-  // This is where authentication happens - app.js (passport.use
+  // This is where authentication happens - app.js
   // authentication locally (not using passport-google, passport-twitter, passport-github...)
-  passport.authenticate('local', { failureRedirect: '/login?message=Incorrect+credentials', failureFlash:true }),
+  passport.authenticate('local', { failureRedirect: 'login?message=Incorrect+credentials', failureFlash:true }),
   function(req, res,next) {
     let prefer = req.user.prefer;
     console.log("fullname: ", prefer);
     res.redirect('/users/profile?name='+prefer); // Successful. redirect to localhost:3000/users/profile
 });
 
+
 router.get('/signup',function(req, res) {
-  // If logged in (req.user exists), go to profile page
-  if(req.user) {  
+  // If logged in, go to profile page
+  if(req.user) {
     let prefer = req.user.prefer;
     return res.redirect('/users/profile?name='+prefer);
   }
